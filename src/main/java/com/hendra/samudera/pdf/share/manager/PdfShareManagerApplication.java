ee322d74ac2cc1b312142790aa66c136f92dd8b2
Kris Hendrawan <hendrazxc@gmail.com>
2025-10-31 23:06:30 +0700

    Initialize PDF Share Manager project with Gradle setup and basic application structure

---
+++ src/main/java/com/hendra/samudera/pdf/share/manager/PdfShareManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hendra.samudera.pdf.share.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfShareManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfShareManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hendra/samudera/pdf/share/manager/PdfShareManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendra.samudera.pdf.share.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfShareManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
